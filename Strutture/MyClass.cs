dbb49ca6f28a4a985f4181baa75b1732bb23ac6b
Stefano <stefano.corda@icloud.com>
2022-02-27 18:20:27 +0100

    Section 12

---
+++ Strutture/MyClass.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Strutture
+{
+    public class MyClass
+    {
+        public string myString;
+        public int myInteger;
+        
+    }
+}
